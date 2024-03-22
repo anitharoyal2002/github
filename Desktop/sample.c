be789bc8e61d4a2608475ba0fe59232968230ec4
anitha <anitharoyal2002@gmail.com>
2024-03-22 12:14:55 +0530

    my first commit

--- Desktop/sample.c
+++ Desktop/sample.c
@@ -1,6 +1,25 @@
 #include<stdio.h>
 int main()
 {
-	printf(" hello world !");
+	int i,*ptr;
+	ptr=(int *)malloc(5*sizeof(int));
+	if(ptr==NULL)
+	{
+		printf("memory not sufficient \n");
+	}
+	else
+	{
+		for(i=0;i<5;i++)
+		{
+			printf("enter the element %d",i+1);
+			scanf("%d",&ptr[i]);
+		}
+	}
+	printf("entered numbers are :\n ");
+	for(i=0;i<5;i++)
+	{
+		printf("%d\t",ptr[i]);
+	}
+
 }
 
